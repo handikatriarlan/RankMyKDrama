5861549b40a0a28277a9ecb36e47e37105c24f04
Arlann <handikaarlan@gmail.com>
2024-11-12 22:17:20 +0700

    Updated image URLs for all KDrama entries in kdramaDatabase.

--- src/data/kdramas.ts
+++ src/data/kdramas.ts
@@ -1,57 +1,57 @@
 export interface KDramaData {
-  title: string
-  image: string
-  year: number
-  genres: string[]
+	title: string
+	image: string
+	year: number
+	genres: string[]
 }
 
 export const kdramaDatabase: KDramaData[] = [
-  {
-    title: 'Goblin',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2016,
-    genres: ['Fantasy', 'Romance', 'Drama'],
-  },
-  {
-    title: 'Descendants of the Sun',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2016,
-    genres: ['Romance', 'Drama', 'Action'],
-  },
-  {
-    title: 'Crash Landing on You',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2019,
-    genres: ['Romance', 'Drama', 'Comedy'],
-  },
-  {
-    title: "It's Okay to Not Be Okay",
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2020,
-    genres: ['Romance', 'Drama', 'Psychological'],
-  },
-  {
-    title: 'Reply 1988',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2015,
-    genres: ['Coming of Age', 'Family', 'Romance'],
-  },
-  {
-    title: 'Hospital Playlist',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2020,
-    genres: ['Medical', 'Drama', 'Friendship'],
-  },
-  {
-    title: 'Mr. Sunshine',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2018,
-    genres: ['Historical', 'Romance', 'Drama'],
-  },
-  {
-    title: 'Signal',
-    image: 'https://asianwiki.com/images/7/7b/Cheer_Up_2022-p1.jpg',
-    year: 2016,
-    genres: ['Crime', 'Thriller', 'Mystery'],
-  },
+	{
+		title: 'Goblin',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2016,
+		genres: ['Fantasy', 'Romance', 'Drama'],
+	},
+	{
+		title: 'Descendants of the Sun',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2016,
+		genres: ['Romance', 'Drama', 'Action'],
+	},
+	{
+		title: 'Crash Landing on You',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2019,
+		genres: ['Romance', 'Drama', 'Comedy'],
+	},
+	{
+		title: "It's Okay to Not Be Okay",
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2020,
+		genres: ['Romance', 'Drama', 'Psychological'],
+	},
+	{
+		title: 'Reply 1988',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2015,
+		genres: ['Coming of Age', 'Family', 'Romance'],
+	},
+	{
+		title: 'Hospital Playlist',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2020,
+		genres: ['Medical', 'Drama', 'Friendship'],
+	},
+	{
+		title: 'Mr. Sunshine',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2018,
+		genres: ['Historical', 'Romance', 'Drama'],
+	},
+	{
+		title: 'Signal',
+		image: 'https://ucarecdn.com/28d4cf05-ac67-43a3-95dc-aa7f245ac99e/lsr1.jpg',
+		year: 2016,
+		genres: ['Crime', 'Thriller', 'Mystery'],
+	},
 ]
